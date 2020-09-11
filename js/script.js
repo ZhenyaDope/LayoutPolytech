74c1352292b430844be60236efaaf835eb06bfad
Evgeny Kolesnikov <zhenyadope@gmail.com>
2020-09-12 01:45:55 +0300

    Добавлена секция со слайдером

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("working...")
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots:true,
+    });
+});
